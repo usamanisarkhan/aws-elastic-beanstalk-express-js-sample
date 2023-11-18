528a5cde9b3dd6334b80e9ee2b5f06ac3f066d54
unknown <usamaniarkhan85@gmail.com>
2023-11-18 01:24:53 -0800

    full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Cloud Pipeline is now Set'));
+app.get('/', (req, res) => res.send('Cloud Pipeline is now reSet'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
